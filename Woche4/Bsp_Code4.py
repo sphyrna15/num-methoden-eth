91df81c8a1c30c4df035ba01002859ab074af6b0
Tim Launer <timmysnakes@gmail.com>
2020-03-11 15:00:14 +0100

    Bsp Code aus der Uebung Woche4

---
+++ Woche4/Bsp_Code4.py
@@ -0,0 +1,27 @@
+""" Bsp Code aus Uebung Woche 4 """
+
+
+""" Reduzieren der Ordnung einer ODE """
+
+""" Right-hand-side """
+# Pendelgleichung Feder
+rhs = lambda y = np.array([y[1, -(k/m) * y[0]]])
+
+
+
+""" Polygonzug Verfahren """
+""" Expliziter Euler """
+
+def exp_euler(f, y0, t0, t_end, N):
+    
+    t, h = np.linspace(t0, t_end, N+1, retstep = True)
+    y = np.zeros((N+1, len(y0))   
+    y[0, :] = y0
+
+    for k in range(N1):
+        y[k+1] = y[k] + h * f(t[k], y[k])
+    
+    return y # y is a Matrix of approximations for the target function 
+
+
+
