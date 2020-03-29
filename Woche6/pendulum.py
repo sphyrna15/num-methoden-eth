318419e8b40fff73595f419d91bc8a35d9e8283b
Tim Launer <timmysnakes@gmail.com>
2020-03-29 15:24:19 +0200

    Bonus Aufgabe pendulum läuft, mu gleich null geht noch nicht

--- Woche6/pendulum.py
+++ Woche6/pendulum.py
@@ -53,16 +53,31 @@ def KL8(Phi_a, Phi_b, y0, t_end, n_steps):
 # --- Problem setup
 
 def phiA(u, dt):
-    # TODO: Implement
-    return 0.
+    # TODO: Implement done
+    q = u[0]
+    p = u[1]
+    t = u[2] + dt
+    return np.array([q + p*dt, p, t])
 
 def phiB(u, dt):
-    # TODO: Implement
-    return 0.
+    # TODO: Implement done
+    q = u[0]
+    if mu == 0:
+       p = u[1] + A*np.sin(omega*u[2]) - dt*(np.sin(u[0])) 
+    else:
+       p = u[1] + (1/mu)*(np.sin(u[0]) - A*np.sin(omega*u[2]))*np.exp(-mu*dt) - (1/mu)*(np.sin(u[0]) - A*np.sin(omega*u[2])) 
+    
+    t = u[2] + dt
+    
+    return np.array([q, p, t])
 
 def energy(y):
     # TODO: Implement
-    return 0.
+    Ekin = (0.5 * y[:,1]**2)
+    Epot =  -np.cos(y[:,0])
+
+    E = Ekin + Epot 
+    return E
 
 # --- Modelparameter ----
 T = 200
@@ -77,10 +92,10 @@ to = 0
 yo = np.array([qo, po, to])
 tspan = [0, T]
 
-# TODO: Implement
+# TODO: Implement done
 
-f = lambda tau,y: 0.
-
+f = lambda tau,y: np.array([y[1], np.sin(y[0]) + A*np.cos(omega*y[2]) - mu*y[1], 1])
+                            
 # RK45
 t45, y45 = ode45(f, tspan, yo)
 
@@ -89,4 +104,98 @@ methods = ['SS', 'PRKS6', 'Y61', 'KL8']
 tSplit = []
 ySplit = []
 
-# TODO: Implement
+# TODO: Implement done
+# Approximiere Pendel mit verschiedenen Verfahren
+for m in methods:
+    print("Approximiere Pendel durch " + m + " mit Mu ungleich null")
+    t, y = splitting_method(phiA, phiB, yo, T, 30000, m)
+    tSplit.append(t)
+    ySplit.append(y)
+
+
+plt.figure()
+plt.plot(t45, y45[:,0], label="RK45")
+plt.title("Mu ungleich null")
+for t, y, m in zip(tSplit, ySplit, methods):
+    plt.plot(t, y[:,0], label = m)
+plt.xlabel("Time")
+plt.ylabel("Auslenkung")
+plt.legend(loc="best")
+plt.grid()
+
+plt.figure()
+plt.plot(t45, energy(y45), label = "RK45")
+for t, y, m in zip(tSplit, ySplit, methods):
+    plt.plot(t, energy(y), label = m)
+plt.xlabel("Time")
+plt.ylabel("Energy, mu nicht null")
+plt.legend(loc="best")
+plt.grid()
+
+
+plt.figure()
+plt.plot(y45[:,0], y45[:,1], label = "RK45")
+for t, y, m in zip(tSplit, ySplit, methods):
+    plt.plot(y[:,0], y[:,1], label = m)
+plt.xlabel("Time")
+plt.ylabel("Trajektorie")
+plt.title("Mu ist nicht null")
+plt.legend(loc="best")
+plt.grid()
+
+
+
+# Setze Mu gleich null und plotte nochmals
+mu = 0
+tSplit2 = []
+ySplit2 = []
+t452, y452 = ode45(f, tspan, yo)
+for m in methods:
+    print("Approximiere Pendel durch " + m + " mit Mu gleich null")
+    t, y = splitting_method(phiA, phiB, yo, T, 30000, m)
+    tSplit2.append(t)
+    ySplit2.append(y)
+    
+
+plt.figure()
+plt.plot(t452, y452[:,0], label="RK45")
+plt.title("Mu gleich null")
+for t, y, m in zip(tSplit2, ySplit2, methods):
+    plt.plot(t, y[:,0], label = m)
+plt.xlabel("Time")
+plt.ylabel("Auslenkung")
+plt.legend(loc="best")
+plt.grid()
+
+
+plt.figure()
+plt.plot(t452, energy(y452), label = "RK45")
+for t, y, m in zip(tSplit2, ySplit2, methods):
+    plt.plot(t, energy(y), label = m)
+plt.xlabel("Time")
+plt.ylabel("Energy, mu null")
+plt.legend(loc="best")
+plt.grid() 
+
+plt.figure()
+plt.plot(y452[:,0], y452[:,1], label = "RK45")
+for t, y, m in zip(tSplit2, ySplit2, methods):
+    plt.plot(y[:,0], y[:,1], label = m)
+plt.xlabel("Time")
+plt.ylabel("Trajektorie")
+plt.title("Mu ist null")
+plt.legend(loc="best")
+plt.grid()
+
+
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
